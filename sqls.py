7d43a689676056c38b8d8268936f528d6d36fc01
Zhen Ju <juzhen@huawei.com>
2022-03-23 11:31:35 +0800

    Get table name from environment var

--- sqls.py
+++ sqls.py
@@ -1,9 +1,10 @@
 import os
 
 CK_DB = os.environ.get('CK_DB')
+CK_TABLE = os.environ.get('CK_TABLE')
 
 CREATE_TABLE_SQL = f'''
-CREATE TABLE IF NOT EXISTS {CK_DB}
+CREATE TABLE IF NOT EXISTS {CK_DB}.{CK_TABLE}
 (
 
     `account` String,
@@ -15,6 +16,7 @@ CREATE TABLE IF NOT EXISTS {CK_DB}
     `status` String
 )
 ENGINE = MergeTree
-ORDER BY account
+ORDER BY token
+PRIMARY KEY token
 SETTINGS index_granularity = 8192;
 '''
